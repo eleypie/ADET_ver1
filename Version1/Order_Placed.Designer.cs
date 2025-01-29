42bd70a2cee707f98daf505659f44f5eaf736b27
Yohairra China Ignacio <151002392+Yohairra@users.noreply.github.com>
2025-01-29 23:17:17 +0800

    gorl

--- Version1/Order_Placed.Designer.cs
+++ Version1/Order_Placed.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(253, 153);
+            this.button1.Location = new System.Drawing.Point(192, 148);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
